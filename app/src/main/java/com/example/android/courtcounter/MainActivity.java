fc5cd99b5a4c409a601288392e508e5f12ccf2c7
glnosg <glina.osg@gmail.com>
2017-08-17 20:59:38 +0200

    Fix bug (restarting layout after changing screen orientation)

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.courtcounter;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setContentView(R.layout.activity_main);
+        displayScoreTeamA(scoreTeamA);
+        displayScoreTeamB(scoreTeamB);
     }
 
     public void addOneTeamA(View view) {
